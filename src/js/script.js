3bdfb68296a498c06ba7490393608d4b3d1afe0b
Rustem Davletov <rustemdavletov7@gmail.com>
2023-02-22 00:20:16 +0300

    Remove slick lib && add tiny-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,29 @@
-$(document).ready(function () {
-    $('.carousel__inner').slick({
-        speed: 500,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    });
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    controls: false,
+    nav: false,
+    responsive: {
+        640: {
+            edgePadding: 20,
+            gutter: 20,
+            items: 1
+        },
+        700: {
+            gutter: 30,
+            items: 1
+        },
+        900: {
+            items: 1
+        }
+    }
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
 });
